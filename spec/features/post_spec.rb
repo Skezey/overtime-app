35979fcb7c33ee346d5efb3d77af1bf2266e6626
Scott Young <youngscottj@gmail.com>
2019-07-31 05:02:09 -0600

    implement posts functionality

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 describe 'navigate' do
@@ -10,6 +12,24 @@ describe 'navigate' do
     it 'has a title of Posts' do
       visit posts_path
       expect(page).to have_content(/Posts/)
+    end
+  end
+
+  describe 'creation' do
+    it 'has a new form that can be reached' do
+      visit new_post_path
+      expect(page.status_code).to eq(200)
+    end
+
+    it 'can be created from new form page' do
+      visit new_post_path
+
+      fill_in 'post[date]', with: Date.today
+      fill_in 'post[rationale]', with: 'some rationale'
+
+      click_on 'Save'
+
+      expect(page).to have_content('some rationale')
     end
   end
 end
