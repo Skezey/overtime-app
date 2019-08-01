d4ebe07e1542c6d049321d461e3842e21d5e8335
Scott Young <youngscottj@gmail.com>
2019-08-01 14:54:44 -0600

    refactor tests to get rid of errors

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,25 +1,25 @@
-# frozen_string_literal: true
-
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  describe 'creation' do
-    before do
-      @user = FactoryGirl.create(:user)
-    end
-    it "can be created" do
-      expect(@user).to be_valid
-    end
-    it "cannot be created without first_name, last_name" do
-      @user.first_name = nil
-      @user.last_name = nil
-      expect(@user).to_not be_valid
-    end
+  before do
+    @user = FactoryGirl.create(:user)
+  end
+
+  describe "creation" do
+  	it "can be created" do
+  		expect(@user).to be_valid
+  	end
+
+  	it "cannot be created without first_name, last_name" do
+  		@user.first_name = nil
+  		@user.last_name = nil
+  		expect(@user).to_not be_valid
+  	end
   end
 
   describe "custom name methods" do
     it 'has a full name method that combines first and last name' do
-      expect(@user.full_name).to eq('SNOW, JOHN')
+      expect(@user.full_name).to eq("SNOW, JON")
     end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,4 @@
 require 'factory_girl_rails'
-FactoryGirl.find_definitions
 
 
 RSpec.configure do |config|
